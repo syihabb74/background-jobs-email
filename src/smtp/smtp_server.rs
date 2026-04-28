5b6810040e33e154bb7d141f4037e097fd7e131b
syihabb74 <syihabb74@gmail.com>
2026-04-28 07:38:36 +0700

    refactor: improve LiveSmtp communication method with enhanced response handling and code formatting

--- src/smtp/smtp_server.rs
+++ src/smtp/smtp_server.rs
@@ -1,4 +1,7 @@
-use std::{io::{BufRead, BufReader, Read, Write}, sync::Arc};
+use std::{
+    io::{BufRead, BufReader, Read, Write},
+    sync::Arc,
+};
 
 use rustls::{ClientConfig, ClientConnection, RootCertStore, StreamOwned};
 use rustls_pki_types::ServerName;
@@ -9,7 +12,7 @@ pub struct SmtpConfig {
 }
 
 impl SmtpConfig {
-    pub fn builder(host: &'static str, port: u16) -> Self {
+    pub fn new(host: &'static str, port: u16) -> Self {
         Self { host, port }
     }
 
@@ -25,45 +28,53 @@ pub struct LiveSmtp<T: Read + Write> {
     stream: T,
 }
 
-impl <T : Read + Write>  LiveSmtp<T> {
-
-    pub fn communicating(&mut self, cmd : &[u8]) -> Result<(), Box<dyn std::error::Error>> {
+impl<T: Read + Write> LiveSmtp<T> {
+    pub fn communicating(&mut self, buff_reader : &mut BufReader<&mut T> ,cmd: &[u8]) -> Result<(), Box<dyn std::error::Error>> {
         Self::write_cmd(&mut self.stream, cmd)?;
-
-        let mut buff_reader = BufReader::new(&mut self.stream);
-        let mut response = String::new();
-        buff_reader.read_line(&mut response)?;
+        loop {
+            let mut response = String::new();
+            match buff_reader.read_line(&mut response) {
+                Ok(0) => break,
+                Ok(_) => {
+                    println!("{}", response);
+                    if response.len() >= 4 && response.chars().nth(3) == Some(' ') {
+                        break;
+                    }
+                }
+                Err(e) => return Err(Box::new(e)),
+            }
+        }
 
         Ok(())
     }
 
-    pub fn write_cmd(stream: &mut impl Write, cmd: &[u8]) -> Result<(),  Box<dyn std::error::Error>> {
+    pub fn write_cmd(
+        stream: &mut impl Write,
+        cmd: &[u8],
+    ) -> Result<(), Box<dyn std::error::Error>> {
         let sending = stream.write_all(cmd)?;
         Ok(sending)
     }
 
-
-
-    pub fn upgrade_tls(self, host : &str) -> Result<LiveSmtp<StreamOwned<ClientConnection, T>>, Box<dyn std::error::Error>> {
-
+    pub fn upgrade_tls(
+        self,
+        host: &str,
+    ) -> Result<LiveSmtp<StreamOwned<ClientConnection, T>>, Box<dyn std::error::Error>> {
         let mut root_store = RootCertStore::empty();
         root_store.extend(webpki_roots::TLS_SERVER_ROOTS.iter().cloned());
 
         let config = Arc::new(
             ClientConfig::builder()
-            .with_root_certificates(root_store)
-            .with_no_client_auth()
+                .with_root_certificates(root_store)
+                .with_no_client_auth(),
         );
 
-        let server_name = ServerName::try_from(host)?
-                                          .to_owned();
+        let server_name = ServerName::try_from(host)?.to_owned();
 
         let conn = ClientConnection::new(config, server_name)?;
 
         Ok(LiveSmtp {
             stream: StreamOwned::new(conn, self.stream),
         })
-
     }
-
 }
